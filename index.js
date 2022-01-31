e1d71b1c6ba1a1c4dc4728a3991ed1e73e295140
singharyan1007 <77246158+singharyan1007@users.noreply.github.com>
2022-01-31 13:45:22 +0530

    landing page added

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const navSlide = () => {
  
     burger.addEventListener("click", () => {
       //Toggle Nav
-      nav.classList.toggle("nav-active");
+    
+      
+        nav.classList.toggle("nav-active");
+      
       //Animate links
       navlinks.forEach((link, index) => {
         if (link.style.animation) {
@@ -23,3 +26,9 @@ const navSlide = () => {
   
 };
 navSlide();
+//intro
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo('.hero',{opacity:0},{opacity:1,duration:1},'-=0.75')
