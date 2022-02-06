0ff49cf1da747ab28d816cfe4189d97c4e2b377c
singharyan1007 <77246158+singharyan1007@users.noreply.github.com>
2022-02-06 23:13:58 +0530

    design patch

--- index.js
+++ index.js
@@ -29,6 +29,56 @@ navSlide();
 //intro
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".slider", { y: "-100%", duration: 1.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
-tl.fromTo('.hero',{opacity:0},{opacity:1,duration:1},'-=0.75')
+// tl.fromTo('.hero',{opacity:0},{opacity:1,duration:1},'-=0.75')
+
+//intersection observers
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-about");
+const sectionTwo = document.querySelector('.section-center');
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px",
+};
+
+// const sectionOneObserver = new IntersectionObserver(function (
+//   entries,
+//   sectionOneObserver
+// ) {
+//   entries.forEach((entry) => {
+//     if (!entry.isIntersecting) {
+//       header.classList.add("nav-scrolled");
+//     } else {
+//       header.classList.remove("nav-scrolled");
+//     }
+//   });
+// },
+// sectionOneOptions);
+
+// sectionOneObserver.observe(sectionOne);
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -200px 0px",
+};
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      // appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+faders.forEach((fader) => {
+  appearOnScroll.observe(fader);
+});
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
+});
