829fb79203343b96385d167fa5f4b6cb3c4b07f1
singharyan1007 <77246158+singharyan1007@users.noreply.github.com>
2022-02-09 16:36:24 +0530

    next patch done

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ const navSlide = () => {
 };
 navSlide();
 //intro
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1.5 });
-tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+// const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+// tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+// tl.to(".slider", { y: "-100%", duration: 1.5 });
+// tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 // tl.fromTo('.hero',{opacity:0},{opacity:1,duration:1},'-=0.75')
 
 //intersection observers
